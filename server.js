7a791e6a629157a88b55fa02f6743965b0b26d06
Justin <justinj.lambeth@gmail.com>
2020-05-27 23:39:14 -0600

    Deploying work in progress

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
 //Dependencies
 const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
